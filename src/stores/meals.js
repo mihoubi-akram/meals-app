3b44bafd4d200facdab0168601ffb0aa067e41ad
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2024-09-04 01:12:52 +0100

    update store meals

--- src/stores/meals.js
+++ src/stores/meals.js
@@ -1,7 +1,17 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 export const useMealsStore = defineStore('Meals', () => {
+    //State
     const meals = ref([])
-    return { meals}
+
+    //Actions
+    const setMeals = (data) => {
+        meals.value = data
+    }
+    const addMeal = (meal)=>{
+        meals.value.push(meal);
+    }
+
+    return { meals,setMeals,addMeal}
 })
   
