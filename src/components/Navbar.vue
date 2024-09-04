911be4b7ff0458133e724865d98a8bad0fbd5acf
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2024-09-04 01:14:33 +0100

    add search bu keyword

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,15 +2,30 @@
     <nav class="navbar">
       <RouterLink :to="{ name: 'home' }" class="navbar-title">Forkify</RouterLink>
       <div class="search-bar">
-        <input type="text" v-model="searchQuery" placeholder="What do you want to cook?"/>
+        <input type="text" v-model="keywords" placeholder="What do you want to cook?" @change="searchMeals"/>
         <svg id="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
         </svg>
       </div>
     </nav>
-  </template>
+</template>
   
   <script setup>
+import axiosClient from '@/axiosClient';
+import { useMealsStore } from '@/stores/meals';
+import { ref } from 'vue';
+
+  const keywords = ref("");
+  const store = useMealsStore();
+  const searchMeals = ()=>{
+    if(keywords.value.trim() != ""){
+      axiosClient.get(`search.php?s=${keywords.value}`)
+        .then(({ data }) => {
+          store.setMeals(data.meals)
+    })
+     
+    }
+  }
   </script>
   
   <style scoped>
